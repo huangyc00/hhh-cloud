9eadd97d0f04ad3ff0efcfd719cb3afe37688f4d
hyc <1057130846@qq.com>
2019-03-09 11:27:07 +0800

    增加ignore文件

--- hhh-consumer/hhh-admin/src/main/java/com/hyc/admin/controller/UserController.java
+++ hhh-consumer/hhh-admin/src/main/java/com/hyc/admin/controller/UserController.java
@@ -3,7 +3,6 @@ package com.hyc.admin.controller;
 import com.hyc.user.api.entity.User;
 import com.hyc.user.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
