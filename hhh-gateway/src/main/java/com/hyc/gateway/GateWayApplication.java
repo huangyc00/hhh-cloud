67263325cce57c6222a2aa0ddae5c8ddbe2ea49b
hyc <1057130846@qq.com>
2019-04-25 17:51:52 +0800

    集成gateway

---
+++ hhh-gateway/src/main/java/com/hyc/gateway/GateWayApplication.java
@@ -0,0 +1,18 @@
+package com.hyc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hyc
+ * @description
+ * @date 2019-04-22 09:52
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
--- hhh-provider/hhh-user/src/main/java/com/hyc/user/service/impl/UserServiceImpl.java
+++ hhh-provider/hhh-user/src/main/java/com/hyc/user/service/impl/UserServiceImpl.java
@@ -2,32 +2,26 @@ package com.hyc.user.service.impl;
 
 import com.hyc.user.api.entity.User;
 import com.hyc.user.api.service.UserService;
-import com.hyc.user.msg.MessageProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserServiceImpl implements UserService {
 
+
     @Override
     public User findById(String id) {
 //        if("1".equals(id)){
-            User user = new User();
-            user.setId("hello world");
-            return user;
+        User user = new User();
+        user.setId("hello world");
+        return user;
 //        }else{
 //            throw new RuntimeException("hahahaha");
 //        }
 
     }
 
-    @Autowired
-    private MessageProvider messageProvider;
-
     @Override
     public String sendUserMsg(User user) {
-        messageProvider.send(user);
         return "success";
     }
 
