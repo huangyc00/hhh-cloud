74e18afb3d85fd4e6ae20a64da499298d879748d
hyc <1057130846@qq.com>
2019-05-02 23:26:43 +0800

    netty 例子

---
+++ hhh-provider/hhh-product/src/main/java/com/hyc/product/netty/echo/EchoServer.java
@@ -0,0 +1,44 @@
+package com.hyc.product.netty.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+
+    private int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap();
+        try {
+            b.group(group).channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.bind().sync();
+            System.out.println(EchoServer.class.getName() + " started and listen on " + f.channel().localAddress());
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoServer(8011).start();
+    }
+}
